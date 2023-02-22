757430afc50fe2080c3c8ef340895389686503f6
yann <yann.thibault1@gmail.com>
2023-02-22 11:38:32 +0100

    add another useless comment

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.18;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // Add useless comment
+// Add another useless comment
 contract MyToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Alyra", "ALY") {
         _mint(msg.sender, initialSupply);
